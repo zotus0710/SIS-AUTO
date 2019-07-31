0f82e5ac81fba1f57b7d411707cbd1eb9fa54e36
zotus0710 <53539576+zotus0710@users.noreply.github.com>
2019-08-01 02:49:40 +0700

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,5 +1,5 @@
 INIT:
-javascript:MA=["112087","688565","112084","690065","112079","689997","112097","688499","112099","688541","112110"]; i=-1;
+javascript:MA=["111881","691573","111861","690461","111868","689915","111875","690725","111883","690386"]; i=-1;
 
 AUTO:
 javascript: i++; CL=document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_btDirectClassRegister_CD"); DC=document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_tbDirectClassRegister_I"); DC.value=MA[i]; CL.click();
